dbe8955c9eeb493f0a5daa72691813d988549c45
Harsh <smartypie0001@gmail.com>
2024-07-18 07:26:09 +0530

    Day1

---
+++ 2/3_Structures.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+
+using namespace std;
+
+// int main()
+// {
+//      // structure is created inside the stack inside the main function.
+//      // Defining a structure doesn't consume any memory. Initializing a structure consumes memory.
+//    struct Cost
+//    {
+//         float costEach;
+//         float quantity;
+//    };
+
+//    struct Cost c1;
+//    c1.costEach = 50;
+//    c1.quantity = 10;
+
+//    cout << "Hunny's total bill is " << "$" << c1.costEach*c1.quantity << endl;
+
+//    struct Cost c2 = {97.5, 2};
+
+//    cout << "Kusum's total bill is " << "$" << c2.costEach*c2.quantity << endl;
+//     return 0;
+// }
+
+
+
+
+// int main()
+// {
+//      struct Student
+//      {
+//           char name[20];
+//           int age;
+//           float cgpa;
+//           float height;
+//      };
+
+//      struct Student s1 = {"Harsh Lakra", 19, 8.17};
+
+//      cout << "Student name is " << s1.name << endl << "His age is " << s1.age << endl << "His CGPA is " << s1.cgpa << endl;
+//      cout << s1.height;
+
+//      /*If we declare a variable and not initialize it (here height), it will be filled with zero in memory inside the stack
+//      belonging to main function */
+     
+// }
+
+
+
+
+
+
+/*We can also make an array of structures. For example, there are 5 employees in a company, and each employee has 3 qualities 
+ like Reg No , Salary, Bank Balance. So 10 structures can be initialized at once */
+
+// int main()
+// {
+//      int serialNumber;
+//      cout << "Enter serial number of employee: ";
+//      cin >> serialNumber;
+
+//      struct qualities
+//      {
+//           int regNo;
+//           int salary;
+//           int bankBalance;
+//      };
+
+//      struct qualities employee[5] = {{101, 10000, 0}, {102, 12000, 14000}, {103, 15000, 100000}};
+
+//      cout << "Registration No: " << employee[serialNumber-1].regNo << endl;
+//      cout << "Salary: " << employee[serialNumber-1].salary << endl;
+//      cout << "Bank Balance: " << employee[serialNumber-1].bankBalance << endl;
+// }
---
+++ 2/4_practiceStructures.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+#include <cstring>
+
+
+
+//We can also define and initialize a structure outside the main function, well in that case the structure will be accessible globally.
+//Also, we can initialize a structure just after declaring it and it's much convenient
+
+struct dish
+    {
+        char name[10];
+        int price;
+        char nationality[10];
+    }d1={"Pizza", 4, "Italian"};
+
+int main()
+{
+     cout << d1.name << endl;
+     cout << d1.price << " dollars" << endl;
+     cout << d1.nationality << endl;
+     cout << endl;
+
+     //checking the size of structure d1
+     cout << sizeof(d1) << endl;
+     cout << endl;
+     /*Output size acc to calculation should be 10+4+10=24 but it's 28. How?
+      name = 10bytes
+      padding = 2bytes          (to make it easier for int to be stored in 4 bytes from 13 to 16, easier for machine to read 4-4 bytes at a time)
+      price = 4bytes
+      nationality = 10 bytes
+      padding = 2 bytes          (to make it 28)
+
+      So, total 28 bytes are occupied
+     */
+
+
+    /*If we want to change the name of the dish we can't directly write d1.name="Churma" as in c++ we can't change the value in a 
+    character variable after initialising it, so we need to use strcpy function which is in <cstring> header file.
+
+    There will be no issue in directly changing the price integer to 1000 like d1.price = 1000 */
+    strcpy(d1.name,"Churma");
+    d1.price = 1000;
+    strcpy(d1.nationality,"Indian");
+
+    cout << d1.name << endl;
+    cout << d1.price << " dollars" << endl;
+    cout << d1.nationality << endl;
+}
